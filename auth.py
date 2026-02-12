7a9ddd1b57f887b89995bce16057c611eaf845ae
sarangali45303-cloud <sarangali45303@gmail.com>
2026-02-12 11:05:19 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -28,7 +28,7 @@ def login_system():
                 else:
                     st.error("Invalid Credentials")
 
-        elif option == "🆕 Register New Shop":
+        elif option == "Register New Shop":
             new_u = st.text_input("Choose Username")
             new_p = st.text_input("Choose Password", type="password")
             new_s = st.text_input("Your Shop Name (e.g. Ali Tailors)")
@@ -80,4 +80,5 @@ def user_profile_ui():
 
     if st.sidebar.button("🚪 Logout", use_container_width=True):
         st.session_state.logged_in = False
+
         st.rerun()
